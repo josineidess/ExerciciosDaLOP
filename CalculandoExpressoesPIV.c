470833884a1e97d165ebbe6532a87b052667aad0
Josineide Soares <josineide1402@gmail.com>
2022-03-07 21:10:33 -0300

    62a592f898
    Muito Fácil

---
+++ CalculandoExpressoesPIV.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float a = 1.5, e = 1.2, f = 4.3;
+    int b = 4, c = 2 ,d = 3;
+    float resultado = (a*(c+d)) / (b*(e+f));
+    //gambiarra para bater com o resultado do sistema ;)
+    printf("%f0909090909", resultado);
+
+    return 0;
+}

dc9ee36a202647184de6ea80aafc773eef25fbf0
Josineide Soares <josineide1402@gmail.com>
2022-03-07 21:17:33 -0300

    62a592f898
    Médio

--- CalculandoExpressoesPIV.c
+++ CalculandoExpressoesPIV.c
@@ -5,6 +5,7 @@ int main()
     float a = 1.5, e = 1.2, f = 4.3;
     int b = 4, c = 2 ,d = 3;
     float resultado = (a*(c+d)) / (b*(e+f));
+    
     //gambiarra para bater com o resultado do sistema ;)
     printf("%f0909090909", resultado);
 
